499c0867679579354f7ac3f9d2994453317b923b
bstamm1 <120616377+bstamm1@users.noreply.github.com>
2022-12-20 20:20:05 -0500

    Use localhost

--- translate.py
+++ translate.py
@@ -6,8 +6,7 @@ import requests
 
 translate_client = translate_v2.Client()
 
-# base_url = 'http://localhost:8080'
-base_url = 'http://54.166.16.48:8080'
+base_url = 'http://localhost:8080'
 collection_id = 'aba58fb2-15df-4713-a851-5118e6c3a254'
 email = 'test@test.edu'
 password = 'admin'
